0f36672af58bb710630733fd21698d9d0f76458b
Kyle Vitale <53164742+KV-CS@users.noreply.github.com>
2022-05-04 10:57:12 -0700

    Create __init__.py

---
+++ new-filters/__init__.py
@@ -0,0 +1,42 @@
+import check50
+import check50.c
+
+filename = "helpers.c"
+@check50.check()
+def helpers_exist():
+    """%s exists.""" % filename
+    check50.exists(filename)
+
+@check50.check(helpers_exist)
+def compiles():
+    """%s compiles.""" % filename
+    check50.c.compile(filename, lcs50=True)
+
+filename2 = "helpers.h"
+@check50.check()
+def helpersh_exist():
+    """%s exists.""" % filename2
+    check50.exists(filename2)
+
+makefile = "MakeFile"
+@check50.check()
+def makefile_exists():
+    """%s exists.""" % makefile
+    check50.exists(makefile)    
+    
+filename3 = "filter.c"
+@check50.check()
+def filterc_exists():
+    """%s exists.""" % filename3
+    check50.exists(filename3)
+
+@check50.check(filterc_exists)
+def compiles():
+    """%s compiles.""" % filename3
+    check50.c.compile(filename3, lcs50=True)
+    
+filename4 = "bmp.h"
+@check50.check()
+def bmph_exist():
+    """%s exists.""" % filename2
+    check50.exists(filename2)
