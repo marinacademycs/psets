da9ba2ddb25c4203d7b6aedf473e45a33030ba94
Kyle Vitale <53164742+KV-CS@users.noreply.github.com>
2022-12-14 16:21:03 -0800

    Update __init__.py

--- caesardecipher/__init__.py
+++ caesardecipher/__init__.py
@@ -12,10 +12,3 @@ def exists():
 def compiles():
     """caesar_decipher.c compiles"""
     check50.c.compile("caesar_decipher.c", lcs50=True)
-
-@check50.check(compiles)
-def check_1():
-    """Handles first paragraph from examples"""
-    x = "Wkh Fdhvdu flskhu lv qdphg diwhu Mxolxv Fdhvdu, zkr, dffruglqj wr Vxhwrqlxv, xvhg lw zlwk d vkliw ri wkuhh (D ehfrplqj G zkhq hqfubswlqj, dqg G ehfrplqj D zkhq ghfubswlqj) wr surwhfw phvvdjhv ri plolwdub vljqlilfdqfh. Zkloh Fdhvdu'v zdv wkh iluvw uhfrughg xvh ri wklv vfkhph, rwkhu vxevwlwxwlrq flskhuv duh nqrzq wr kdyh ehhq xvhg hduolhu. (iurp Zlnlshgld)"
-    y = "plaintext: The Caesar cipher is named after Julius Caesar, who, according to Suetonius, used it with a shift of three (A becoming D when encrypting, and D becoming A when decrypting) to protect messages of military significance. While Caesar's was the first recorded use of this scheme, other substitution ciphers are known to have been used earlier. (from Wikipedia)"
-    check50.run("./caesar_decipher").stdin(x).stdout(y).exit(0)
